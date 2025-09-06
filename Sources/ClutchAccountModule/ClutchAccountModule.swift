7db9cad3a78b26dbc4c502baa34eaa1332bbb3b7
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-09-07 00:28:42 +0300

    added modular protocols

--- Sources/ClutchAccountModule/ClutchAccountModule.swift
+++ Sources/ClutchAccountModule/ClutchAccountModule.swift
@@ -1,2 +1,31 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+import ClutchModularProtocols
+
+
+//MARK: ClutchAccountModule
+@MainActor
+public class ClutchAccountModule : @preconcurrency AccountModuleProtocol {
+    public init() { }
+  public func createAccountModule() -> AnyView {
+        let viewController = LoginRouter.createLoginViewController()
+      return AnyView(ViewControllerWrapper(viewController: viewController))
+    }
+    
+}
+
+
+
+
+struct ViewControllerWrapper: UIViewControllerRepresentable {
+    let viewController: UIViewController
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+       
+    }
+}
